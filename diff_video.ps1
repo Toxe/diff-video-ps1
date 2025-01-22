2b120f7a68526cbd334eb1a36a63dd3722a8748c
Toxe <toxe@toxe.net>
2025-01-22 13:17:27 +0100

    Rename _diff and _norm PNGs to _d and _n

--- diff_video.ps1
+++ diff_video.ps1
@@ -118,7 +118,7 @@ function GenerateDiffs {
     1..$number_of_frames | ForEach-Object -ThrottleLimit $num_cores -Parallel {
         $id = "{0:d6}" -f $_
         $frame = Join-Path -Path "${using:work_dir}" -ChildPath "${id}"
-        magick -limit thread $using:imagick_threads "${frame}_a.png" "${frame}_b.png" -compose difference -composite -evaluate Pow 2 -evaluate divide 3 -separate -evaluate-sequence Add -evaluate Pow 0.5 "${frame}_diff.png"
+        magick -limit thread $using:imagick_threads "${frame}_a.png" "${frame}_b.png" -compose difference -composite -evaluate Pow 2 -evaluate divide 3 -separate -evaluate-sequence Add -evaluate Pow 0.5 "${frame}_d.png"
 
         $q = $using:frames_completed
         $q.Enqueue(1)
@@ -144,7 +144,7 @@ function CalculateMinMaxIntensity {
     $lines = 1..$number_of_frames | ForEach-Object -ThrottleLimit $num_cores -Parallel {
         $id = "{0:d6}" -f $_
         $frame = Join-Path -Path "${using:work_dir}" -ChildPath "${id}"
-        magick identify -limit thread $using:imagick_threads -format "%[min] %[max]\n" "${frame}_diff.png"
+        magick identify -limit thread $using:imagick_threads -format "%[min] %[max]\n" "${frame}_d.png"
 
         $q = $using:frames_completed
         $q.Enqueue(1)
@@ -185,7 +185,7 @@ function NormalizeDiffs {
     1..$number_of_frames | ForEach-Object -ThrottleLimit $num_cores -Parallel {
         $id = "{0:d6}" -f $_
         $frame = Join-Path -Path "${using:work_dir}" -ChildPath "${id}"
-        magick -limit thread $using:imagick_threads "${frame}_diff.png" -level "$using:min_intensity,$using:max_intensity" "${frame}_norm.png"
+        magick -limit thread $using:imagick_threads "${frame}_d.png" -level "$using:min_intensity,$using:max_intensity" "${frame}_n.png"
 
         $q = $using:frames_completed
         $q.Enqueue(1)
@@ -205,7 +205,7 @@ function RenderOutputVideo {
     Write-Host "rendering output video..."
 
     $t0 = Get-Date
-    ffmpeg -v error -framerate 60000/1001 -i "$work_dir\%06d_norm.png" -vf "colorchannelmixer=.0:.0:.0:0:.0:1:.0:0:.0:.0:.0:0" -c:v libx264 -crf 18 -preset veryfast "$output_video"
+    ffmpeg -v error -framerate 60000/1001 -i "$work_dir\%06d_n.png" -vf "colorchannelmixer=.0:.0:.0:0:.0:1:.0:0:.0:.0:.0:0" -c:v libx264 -crf 18 -preset veryfast "$output_video"
     ShowDuration $t0
 }
 
